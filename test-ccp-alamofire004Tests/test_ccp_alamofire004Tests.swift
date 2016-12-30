a82779057ce8675df1156a30b2becd5e2d3fd230
davidkneely <deeniri@gmail.com>
2016-12-29 18:33:49 -1000

    Initial Commit

---
+++ test-ccp-alamofire004Tests/test_ccp_alamofire004Tests.swift
@@ -0,0 +1,36 @@
+//
+//  test_ccp_alamofire004Tests.swift
+//  test-ccp-alamofire004Tests
+//
+//  Created by pair on 12/29/16.
+//  Copyright © 2016 pair. All rights reserved.
+//
+
+import XCTest
+@testable import test_ccp_alamofire004
+
+class test_ccp_alamofire004Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

d75e41575b3ca2e36ddd3806af8e88620df51639
davidkneely <deeniri@gmail.com>
2016-12-30 07:01:18 -1000

    (build):First commit

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'test-ccp-alamofire004' do
+
+  pod 'Alamofire', '~> 4.2'
+
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for test-ccp-alamofire004
+
+  target 'test-ccp-alamofire004Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- test-ccp-alamofire004/ViewController.swift
+++ test-ccp-alamofire004/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let user = "acc_c954e54d37e4be9"
+        let password = "0d2f9fcbbdda702b59e7795d1237e382"
+        
+        var headers: HTTPHeaders = [:]
+        
+        if let authorizationHeader = Request.authorizationHeader(user: user, password: password) {
+            headers[authorizationHeader.key] = authorizationHeader.value
+            //headers["Accept":"application/json"]
+            
+        }
+        
+        Alamofire.request("https://api.imagga.com/v1/tagging?url=http://thehorseaholic.com/wp-content/uploads/2015/08/bigstock-Child-riding-a-horse-in-meadow-44787883-2.jpg", headers: headers)
+            .responseJSON { response in
+                debugPrint(response)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
